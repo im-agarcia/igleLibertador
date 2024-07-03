80735180df9749cf3dea05a2655b72b2d01fac0d
im-agarcia <im.agarcia@outlook.com>
2024-07-02 22:26:56 -0300

    primera subida

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import PWABadge from './PWABadge.jsx'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <p>Tostadora</p>
+      <PWABadge />
+    </>
+  )
+}
+
+export default App
